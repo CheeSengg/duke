48319dbd680d643e584fd0c1ebc82e3b824920f2
cheesengg <E0309953@u.nus.edu>
2019-09-01 19:48:35 +0800

    Added: Duke_Response.java to store Duke's common response

---
+++ src/main/java/Duke/Constant/Duke_Response.java
@@ -0,0 +1,26 @@
+package Duke.Constant;
+
+public class Duke_Response {
+
+    public final static String SPACES = "   __________________________________\n";
+
+    public final static String GREET = "    Hello! I'm Duke\n    What can I do for you?\n";
+
+    public final static String BYE = "    Bye. Hope to see you again soon!\n";
+
+    public final static String LIST_FOUND = "    Here are the tasks in your list:\n";
+
+    public final static String LIST_NOT_FOUND = "    Sorry! Your List is currently empty:\n";
+
+    public final static String DONE_FOUND = "    Nice! I've marked this task as done:\n";
+
+    public final static String DELETE_FOUND = "    Noted. I've removed this task:\n";
+
+    public final static String FIND_FOUND = "    Here are the matching tasks in your list:\n";
+
+    public final static String NOT_FOUND = "    Sorry! The task cannot be found: \n";
+
+    public final static String EXCEPTION = "    ☹ OOPS!!! That is an invalid input\n    Please try again. \n";
+
+    public final static String ADD = "    Got it. I've added this task:\n";
+}
